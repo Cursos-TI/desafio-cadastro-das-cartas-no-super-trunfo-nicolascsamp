9dae85a30107349978689f472ccd3a0bacf0c4c3
Nicolas Sampaio <73140256+nicolascsamp@users.noreply.github.com>
2025-03-19 11:58:22 -0300

    [Update] CartasSuperTrunfo.c
    Super Trunfo Estácio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,49 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+ // Formulário de cadastro de país
+ int codigo, area, turistico;
+ int pib;
+ int populacao;
+ char nome[100];
 
-    return 0;
+ // Boas-vindas
+ printf("Bem-Vindo ao Super Trunfo!\n");
+
+ // Perguntas
+
+ printf("Digite o nome do país: \n");
+ scanf("%s", nome);
+ 
+ printf("Digite o código do país: \n");
+ scanf("%d", &codigo);
+
+ printf("Digite a área do país: \n");
+ scanf("%d", &area);
+
+ printf("Digite o PIB do país: \n");
+ scanf("%d", &pib);
+
+ printf("Digite a população do país: \n");
+ scanf("%d", &populacao);
+
+ printf("Quantos pontos turísticos o país tem: \n");
+ scanf("%d", &turistico);
+
+ // Divisão de valores
+ float divisao1 = (float)populacao / area;
+ float divisao2 = (float)pib / populacao;
+
+ printf("País: %s\n", nome);
+ printf("Área: %d\n", area);
+ printf("Código do país: %d\n", codigo);
+ printf("PIB: %d\n", pib);
+ printf("População: %d\n", populacao);
+ printf("Pontos turísticos: %d\n", turistico);
+ printf("População / Área: %.2f\n", divisao1);
+ printf("PIB per Capita: %.2f\n", divisao2);
+
+ return 0;
+
 }
